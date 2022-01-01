8466294c0f61fcb4e46a40558eb7d7b242148d7c
照井拓朗 <>
2022-01-01 21:16:18 +0900

    カスタムディレクトリ作成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,22 @@
 <template>
-  <p v-border>home</p>
+  <p v-border:dotted.round.shadow="{width: '5px', color: 'red'}">home</p>
 </template>
+
+<script>
+// カスタムディレクティブ（ローカル）
+export default {
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px";
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,23 +4,18 @@ import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive('border', {
-  bind(el, binding, vnode) {
-    // ディレクティブがはじめて対象の要素に紐づいて時
-  },
-  inserted(el, binding, vnode) {
-    // 親Nodeに挿入された時
-  },
-  update(el, binding, vnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新される前
-  },
-  componentUpdated(el, binding, vnode, oldVnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新される後
-  },
-  unbind(el, binding, vnode, oldVnode) {
-    // ディレクティブが紐づいている要素から取り除かれた時
-  },
-});
+// カスタムディレクティブ（グローバル）
+// Vue.directive('border', function(el, binding) {
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem";
+//   }
+//   if (binding.modifiers.shadow) {
+//     el.style.boxShadow = "0 2px 5px";
+//   }
+// });
 
 new Vue({
   render: h => h(App),
