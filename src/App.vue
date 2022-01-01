565acf6de7ba9aa303b7afcbb5024bb9694e5398
照井拓朗 <>
2022-01-01 16:45:42 +0900

    v-slot

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <h2>{{ number }}</h2>
+    <LikeHeader>
+      <h2>みなさん</h2>
+      <template #title="slotProps">
+        <h2>こんにちは</h2>
+        <h2>{{ slotProps.user.firstName }}</h2>
+      </template>
+      <h3>はじめまして</h3>
+      <p>よろしくお願いします</p>
+      <template #number>
+        <p>{{ number }}</p>
+      </template>
+    </LikeHeader>
     <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
     <LikeNumber :total-number="number"></LikeNumber>
   </div>
