9f59a616b2136e698263dced7b97bf4fe742ebcb
照井拓朗 <>
2022-01-01 20:38:20 +0900

    v-modelでフォーム作成

---
+++ src/components/EventTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <duv>
+    <label for="title">タイトル</label>
+    <input
+      id="title"
+      type="text"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    >
+    <pre>{{ value }}</pre>
+  </duv>
+</template>
+
+<script>
+export default {
+  props: ['value']
+};
+</script>
